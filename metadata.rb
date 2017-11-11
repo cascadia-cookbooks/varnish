f6d57310f7dba73db2b7322d293e5ef218fdb291
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-10 21:04:25 -0800

    0.1.0

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'cas_varnish'
+maintainer 'Cascadia Cookbooks'
+maintainer_email 'null'
+license 'GPL-3.0'
+description 'Manges system cron daemon'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+source_url 'https://github.com/cascadia-cookbooks/cron'
+issues_url 'https://github.com/cascadia-cookbooks/cron/issues'
+
+supports 'ubuntu', '>=16.04'
+supports 'rhel', '>=7'
+supports 'centos', '>=7'
+supports 'debian', '>=8'
