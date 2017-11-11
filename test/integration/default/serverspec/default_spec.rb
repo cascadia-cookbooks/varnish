6225fc75196bb9f1d9d623bcffe59e9ac92f8ab0
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-10 21:10:48 -0800

    Test for service

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,4 +4,8 @@ describe 'cas_varnish::default' do
     describe package('varnish') do
         it { should be_installed }
     end
+
+    describe service('varnish') do
+        it { should be_running }
+    end
 end
