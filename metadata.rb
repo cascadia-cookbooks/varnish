ac0265a1ffc062d8014038016cb434939c4e523a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-11 12:21:31 -0800

    0.1.0

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,14 @@ name 'cas_varnish'
 maintainer 'Cascadia Cookbooks'
 maintainer_email 'null'
 license 'GPL-3.0'
-description 'Manges system cron daemon'
+description 'Manages Varnish HTTP cache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-source_url 'https://github.com/cascadia-cookbooks/cron'
-issues_url 'https://github.com/cascadia-cookbooks/cron/issues'
+source_url 'https://github.com/cascadia-cookbooks/varnish'
+issues_url 'https://github.com/cascadia-cookbooks/varnish/issues'
+
+depends 'yum-epel'
 
 supports 'ubuntu', '>=16.04'
 supports 'rhel', '>=7'
