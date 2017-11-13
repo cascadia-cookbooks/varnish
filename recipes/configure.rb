543669c915b17ff02ecb6fdc6eb974c4ff94aea2
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-12 17:14:36 -0800

    Apply secret attribute to file if defined

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -4,6 +4,15 @@
 # Description:: Configures Varnish HTTP cache 
 #
 
+if node['cas_varnish']['secret']
+    file '/etc/varnish/secret' do
+        owner 'root'
+        group 'root'
+        mode 0600
+        content node['cas_varnish']['secret']
+    end
+end
+
 service 'varnish' do
     action [:enable, :start]
 end
