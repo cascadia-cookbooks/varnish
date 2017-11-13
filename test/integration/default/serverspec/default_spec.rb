3bbbc017e4860b907a4f24b3db195f041b03c5de
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-12 17:12:20 -0800

    Test for secret file

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -9,3 +9,13 @@ describe 'cas_varnish::default' do
         it { should be_running }
     end
 end
+
+describe 'cas_varnish::configure' do
+    describe file('/etc/varnish/secret') do
+        it { should exist }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 600 }
+        its(:content) { should include 'secretattribute' }
+    end
+end
