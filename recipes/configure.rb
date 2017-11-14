0e14a131784ea03979419c8ead2b642ba1d5025e
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-13 18:18:10 -0800

    Formatting

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -6,9 +6,9 @@
 
 if node['cas_varnish']['secret']
     file '/etc/varnish/secret' do
-        owner 'root'
-        group 'root'
-        mode 0600
+        owner   'root'
+        group   'root'
+        mode    0600
         content node['cas_varnish']['secret']
     end
 end
--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -8,21 +8,21 @@ case node['platform_family']
 when 'debian'
 
     apt_repository 'varnish' do
-        uri "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/#{node['platform']}"
+        uri          "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/#{node['platform']}"
         distribution "#{node['lsb']['codename']}"
-        key "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/gpgkey"
-        components ['main']
+        key          "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/gpgkey"
+        components   ['main']
     end
 
 when 'rhel'
 
     yum_repository 'varnish' do
         description 'Varnish Repository - Packagecloud'
-        baseurl "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/el/#{node['platform_version'].to_i}/$basearch/"
-        enabled true
-        gpgcheck false
-        gpgkey "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/gpgkey"
-        action :create
+        baseurl     "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/el/#{node['platform_version'].to_i}/$basearch/"
+        enabled     true
+        gpgcheck    false
+        gpgkey      "https://packagecloud.io/varnishcache/varnish#{node['cas_varnish']['version']}/gpgkey"
+        action      :create
     end
 
 end
